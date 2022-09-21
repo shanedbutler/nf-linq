eb5974b8b73351c6b5a8186ca447a43cedeaf51d
shanedbutler <shanedbutler@gmail.com>
2022-09-21 14:38:44 -0400

    Complete LINQ Ordering Operations

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace linqd
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Restriction/Filtering Operations:\n");
+
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             // Find the words in the fruits collection that start with the letter 'L'
@@ -38,6 +40,38 @@ namespace linqd
             {
                 Console.WriteLine(number);
             }
+
+            Console.WriteLine("\nOrdering Operations:");
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            // Order names in descending order. ASK: What is the explicit type of descendingNames var? (linq method)
+            var descendingNames = names.OrderByDescending(n => n);
+            Console.WriteLine("\nStrings sorted in descending order:");
+            foreach (var name in descendingNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            // Build a collection of these numbers sorted in ascending order (linq query)
+            List<int> numbers2 = new List<int>() {15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96};
+            var descendingNumbers = from number in numbers
+                                     orderby number ascending
+                                     select number; 
+            Console.WriteLine("\nNumbers sorted in ascending order:");
+            foreach (var number in descendingNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
         }
         //Method to filter strings in collection by their first letter
         public static List<string> FilterByFirstLetter(List<string> collection, string letter)
