ad41eea5fdbe42edf21cf3f46b31d935b66b830a
shanedbutler <shanedbutler@gmail.com>
2022-09-21 14:07:09 -0400

    Initialize project and add first query / method

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            // Find the words in the fruits collection that start with the letter 'L'
+            List<string> filteredFruits = FilterByFirstLetter(fruits, "L");
+            Console.WriteLine("Filtered by my own method:");
+            foreach (string fruit in filteredFruits)
+            {
+                Console.WriteLine(fruit);
+            }
+            // Find the words in the fruits collection that start with the letter 'L' (linq query)
+            IEnumerable<string> results = from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            Console.WriteLine("\nFiltered with LINQ query:");
+            foreach (string result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+
+        }
+        //Method to filter strings in collection by their first letter
+        public static List<string> FilterByFirstLetter(List<string> collection, string letter)
+        {
+            return collection.FindAll(item => item.StartsWith(letter));
+        }
+    }
+}
